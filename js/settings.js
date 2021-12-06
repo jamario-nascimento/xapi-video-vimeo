71a16d1131620ee33187c70cec5ab152ae196517
Jamario Nascimento <jamariobatista@gmail.com>
2021-12-06 15:16:03 -0300

    adicionado conteudo inicial

---
+++ js/settings.js
@@ -0,0 +1,17 @@
+// LRS Connection
+let scormReporting = false;
+let xAPIReporting = true;
+let kerRef = "386f1ebb26e7408b453076ddb131e98c31208eff";
+let secretRef = "b33ed477dfe2a92d63686e679e0855647086b9ca";
+let endPointRef = "https://146.148.37.167/data/xAPI"
+
+//Reporting Variables
+let objectID = "https://www.learningdojo.net/xapi/course1";
+let objectName = "Video Vímeo";
+
+// Display labels
+let videoID = "464849280";
+let title = 'Exemplo de video vimeo';
+let description = "Este exemplo visa contruir uma experiência de aprendizado com o uso de um video vimeo.";
+let copyrightText = '&#169; 2020 Learning Dojo';
+let profilePic = 'media/me.jpg';
